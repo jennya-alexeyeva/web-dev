4a7b7608dc2189e654e987d3cf6b6d12584d4851
jennya alexeyeva <jennyaalexeyeva@gmail.com>
2022-03-21 23:37:20 -0400

    lets try this again :)

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HelloWorld from './components/hello-world';
 import Labs from './components/labs';
 import Tuiter from './components/tuiter';
-import('./vendors/bootstrap/bootstrap.min.css');
-import('./vendors/bootstrap/css/bootstrap.min.css');
-import('https://kit.fontawesome.com/59c24dac4b.js');
+import './vendors/bootstrap/bootstrap.min.css';
+import './vendors/bootstrap/css/bootstrap.min.css';
+import 'https://kit.fontawesome.com/59c24dac4b.js';
 
 function App() {
   return (
